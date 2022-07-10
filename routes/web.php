7412ee9c905ca8db75eea451cd1725c915861c72
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-07-10 12:42:35 -0300

    Adiciona method, view e middleware de login

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\EpisodesController;
+use App\Http\Controllers\loginController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use App\Http\Middleware\Autenticador;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,9 +31,12 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 //Route::post('/series/destroy/{id}', [SeriesController::class, 'destroy'])->name('series.destroy');
-Route::get('/', [SeriesController::class, 'index']);
+Route::get('/', [SeriesController::class, 'index'])->middleware(Autenticador::class);
 Route::resource('/series', SeriesController::class);
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 //->only(['index', 'create', 'store', 'delete']);
 Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
 Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'watched'])->name('episodes.watched');
+
+Route::get('/login', [loginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('signIn');
